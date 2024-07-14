5aa7667c459bc6040a0334eb2409cf21f3fddd0d
Eyal Pano <eyal.pano@gmail.com>
2024-07-14 12:19:59 +0300

    update to clean

--- dist/routes/photoRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const photoController_1 = require("../controllers/photoController");
-const router = (0, express_1.Router)();
-router.get('/:count', photoController_1.getRandomPhotos);
-exports.default = router;
